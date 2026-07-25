42f4f89482a70d07b1e791c309c8f4f1bfe91f47
Yaner De Leon <yanerdl80@gmail.com>
2026-07-24 18:42:29 -0600

    Permitir diagnostico temporal en Render

--- torneo/settings.py
+++ torneo/settings.py
@@ -32,7 +32,12 @@ SECRET_KEY = os.environ.get(
 #DEBUG = True
 #DEBUG = os.environ.get("DEBUG", "True").lower() == "true"
 #DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
-DEBUG = os.environ.get("RENDER") is None
+#DEBUG = os.environ.get("RENDER") is None
+
+DEBUG = os.environ.get(
+    "DEBUG",
+    "False" if os.environ.get("RENDER") else "True"
+).lower() == "true"
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
